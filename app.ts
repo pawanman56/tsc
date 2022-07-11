3d8a7065acfb6c5f3ab1b0e49be731e87418355f
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-11 17:15:50 +0545

    initial tsc commit

---
+++ app.ts
@@ -0,0 +1,8 @@
+let message: string = 'Hello World!';
+
+// create h1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+// add h1 to document
+document.body.appendChild(heading);
