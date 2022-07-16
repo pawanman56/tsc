dcf64c0b45e57241e1306e2531e7623446e1bc13
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-16 23:23:01 +0545

    abstract class

--- inheritance.js
+++ inheritance.js
@@ -1,42 +1,77 @@
-var User = /** @class */ (function () {
-    function User(firstName, lastName) {
+class User {
+    constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    User.prototype.getFullName = function () {
-        return "".concat(this.firstName, " ").concat(this.lastName);
-    };
-    User.prototype.describe = function () {
-        return "Full name is ".concat(this.firstName, " ").concat(this.lastName);
-    };
-    return User;
-}());
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    describe() {
+        return `Full name is ${this.firstName} ${this.lastName}`;
+    }
+}
 // class Employee extends User {
-var Employee = /** @class */ (function () {
-    function Employee(firstName, lastName, jobTitle) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.jobTitle = jobTitle;
-        Employee.headCount++;
-    }
-    /*constructor(
-        firstName: string,
-        lastName: string,
-        private jobTitle: string
-    ) {
-        super(firstName, lastName);
-    }
+// class Employee {
+/*constructor(
+    firstName: string,
+    lastName: string,
+    private jobTitle: string
+) {
+    super(firstName, lastName);
+}
 
-    describe(): string {
-        return super.describe() + `. I'm a ${this.jobTitle}.`;
-    }*/
-    Employee.headCount = 0;
-    return Employee;
-}());
-var john = new Employee('John', 'Doe', 'Web Developer');
-var jane = new Employee('Jane', 'Doe', 'Web Developer');
+describe(): string {
+    return super.describe() + `. I'm a ${this.jobTitle}.`;
+}*/
+/*public static headCount: number = 0;
+
+constructor(
+    private firstName: string,
+    private lastName: string,
+    private jobTitle: string
+) {
+    Employee.headCount++;
+}
+}*/
+// let john = new Employee('John', 'Doe', 'Web Developer');
+// let jane = new Employee('Jane', 'Doe', 'Web Developer');
 // console.log(employee.getFullName());
 // console.log(employee.describe());
-console.log(Employee.headCount);
+// console.log(Employee.headCount);
+class Employee {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    compensationStatement() {
+        return `${this.fullName} makes ${this.getSalary()} a month.`;
+    }
+}
+class FullTimeEmployee extends Employee {
+    constructor(firstName, lastName, salary) {
+        super(firstName, lastName);
+        this.salary = salary;
+    }
+    getSalary() {
+        return this.salary;
+    }
+}
+class Contractor extends Employee {
+    constructor(firstName, lastName, rate, hours) {
+        super(firstName, lastName);
+        this.rate = rate;
+        this.hours = hours;
+    }
+    getSalary() {
+        return this.rate * this.hours;
+    }
+}
+let john = new FullTimeEmployee('John', 'Doe', 12000);
+let jane = new Contractor('Jane', 'Doe', 100, 160);
+console.log(john.compensationStatement());
+console.log(jane.compensationStatement());
--- inheritance.ts
+++ inheritance.ts
@@ -14,7 +14,7 @@ class User {
 }
 
 // class Employee extends User {
-class Employee {
+// class Employee {
     /*constructor(
         firstName: string,
         lastName: string,
@@ -27,7 +27,7 @@ class Employee {
         return super.describe() + `. I'm a ${this.jobTitle}.`;
     }*/
 
-    public static headCount: number = 0;
+    /*public static headCount: number = 0;
 
     constructor(
         private firstName: string,
@@ -36,12 +36,52 @@ class Employee {
     ) {
         Employee.headCount++;
     }
-}
+}*/
 
-let john = new Employee('John', 'Doe', 'Web Developer');
-let jane = new Employee('Jane', 'Doe', 'Web Developer');
+// let john = new Employee('John', 'Doe', 'Web Developer');
+// let jane = new Employee('Jane', 'Doe', 'Web Developer');
 
 // console.log(employee.getFullName());
 // console.log(employee.describe());
 
-console.log(Employee.headCount);
+// console.log(Employee.headCount);
+
+abstract class Employee {
+    constructor(private firstName: string, private lastName: string) {}
+
+    abstract getSalary(): number
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    compensationStatement(): string {
+        return `${this.fullName} makes ${this.getSalary()} a month.`;
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    constructor(firstName: string, lastName: string, private salary: number) {
+        super(firstName, lastName);
+    }
+
+    getSalary(): number {
+        return this.salary;
+    }
+}
+
+class Contractor extends Employee {
+    constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
+        super(firstName, lastName);
+    }
+
+    getSalary(): number {
+        return this.rate * this.hours;
+    }
+}
+
+let john = new FullTimeEmployee('John', 'Doe', 12000);
+let jane = new Contractor('Jane', 'Doe', 100, 160);
+
+console.log(john.compensationStatement());
+console.log(jane.compensationStatement());
