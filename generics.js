6482e49bfd87fd761b2521af0aa98e0e30603cae
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-17 18:42:24 +0545

    generic function

--- generics.js
+++ generics.js
@@ -6,3 +6,10 @@ var numbers = [1, 2, 8, 3, 0, 4, 5];
 var colors = ['red', 'green', 'blue'];
 console.log(getRandomNumberElement(numbers));
 console.log(getRandomNumberElement(colors));
+function getRandomElement(items) {
+    var randomIndex = Math.floor(Math.random() * items.length);
+    return items[randomIndex];
+}
+var numArry = [1, 5, 7, 13];
+var randomElm = getRandomElement(numArry);
+console.log(randomElm);
--- generics.ts
+++ generics.ts
@@ -8,3 +8,13 @@ let colors = ['red', 'green', 'blue'];
 
 console.log(getRandomNumberElement(numbers));
 console.log(getRandomNumberElement(colors));
+
+function getRandomElement<T>(items: T[]): T {
+    let randomIndex = Math.floor(Math.random() * items.length);
+    return items[randomIndex];
+}
+
+let numArry = [1, 5, 7, 13];
+let randomElm = getRandomElement<number>(numArry);
+
+console.log(randomElm);
