8ba94e4c22ffdeaa8d6871cf74c6c38b29ff2da4
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-17 15:49:52 +0545

    type guard

---
+++ type-guard.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    throw new Error('Invalid arguments. Both arguments must be either numbers or strings.');
+}
+console.log(add('pawan', 100));
---
+++ type-guard.ts
@@ -0,0 +1,15 @@
+type alphanumeric = string | number;
+
+function add(a:alphanumeric, b: alphanumeric) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+
+    throw new Error('Invalid arguments. Both arguments must be either numbers or strings.');
+}
+
+console.log(add('pawan', 'sunuwar'));
