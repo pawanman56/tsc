e6b5b38b5836dd5e01b6f54426786c99ac3071d0
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-17 22:59:22 +0545

    generic interfaces

---
+++ generic-interface.ts
@@ -0,0 +1,50 @@
+// interface describing object properties
+interface Pair<K, V> {
+    key: K,
+    value: V
+}
+
+let month: Pair<string, number> = {
+    key: 'Jan',
+    value: 1
+}
+
+console.log(month);
+
+// interface describing methods
+interface Collection<T> {
+    add(o: T): void,
+    remove(o: T): void
+}
+
+class List<T> implements Collection<T> {
+    private items: T[] = [];
+
+    add(o: T): void {
+        this.items.push(o);
+    }
+
+    remove(o: T): void {
+        let index = this.items.indexOf(o);
+
+        if (index > -1) {
+            this.items.splice(index, 1);
+        }
+    }
+}
+
+let list = new List<number>();
+
+for (let i = 0; i < 10; i++) {
+    list.add(i);
+}
+
+// interface describing index types
+interface Options<T> {
+    [name: string]: T
+}
+
+let inputOptions: Options<boolean> = {
+    'disabled': false,
+    'visible': true
+}
