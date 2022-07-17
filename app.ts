872053c2c737253145a188c697c27cbce4f78104
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-17 23:38:18 +0545

    default export

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import { EmailValidator } from './modules/EmailValidator';
+import ZipCodeValidator from './modules/ZipCodeValidator';
 
 let message: string = 'Hello World!';
 
@@ -11,7 +12,12 @@ let email = 'john.doe@testmail.com';
 let emailValidator = new EmailValidator();
 let res = emailValidator.isValid(email);
 
+// use ZipCodeValidator
+let zipValidator = new ZipCodeValidator();
+let zipRes = zipValidator.isValid('44660');
+
 console.log(res);
+console.log(zipRes);
 
 // add h1 to document
 document.body.appendChild(heading);
